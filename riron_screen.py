9102a6dc07fb298f58cdb89c7269c0ad4310a651
ichikon77 <ichikon77@yahoo.co.jp>
2026-08-16 10:21:46 +0900

    update riron report 2026-08-16

--- riron_screen.py
+++ riron_screen.py
@@ -338,6 +338,7 @@ HTML_HEAD = """<!DOCTYPE html>
     <a href="buffett.html" style="border-color:#db2777">バフェット</a>
     <a href="cramer.html" style="border-color:#db2777">クレイマー</a>
     <a href="kijitsu.html" style="border-color:#db2777">信用期日</a>
+    <a href="kijitsu_us.html" style="border-color:#db2777">信用期日(US)</a>
     <a href="kasetsu.html" style="border-color:#94a3b8">仮説検証</a>
   </nav>
   <h1>日経平均 理論株価（PER・PBRレンジ）</h1>
