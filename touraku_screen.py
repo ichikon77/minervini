7d4fea0ce8901beeb7eb03bcde1ec13549ec2fd0
ichikon77 <ichikon77@yahoo.co.jp>
2026-08-16 10:22:57 +0900

    update touraku report 2026-08-16

--- touraku_screen.py
+++ touraku_screen.py
@@ -261,6 +261,7 @@ HTML_TEMPLATE = """<!DOCTYPE html>
     <a href="buffett.html" style="border-color:#db2777">バフェット</a>
     <a href="cramer.html" style="border-color:#db2777">クレイマー</a>
     <a href="kijitsu.html" style="border-color:#db2777">信用期日</a>
+    <a href="kijitsu_us.html" style="border-color:#db2777">信用期日(US)</a>
     <a href="kasetsu.html" style="border-color:#94a3b8">仮説検証</a>
   </nav>
   <h1>騰落レシオ</h1>
